b5b257c53dd0587ee47db35763d6699a22692f05
anHTMLprogrammer <themostordinaryearthling@gmail.com>
2025-05-21 18:14:32 +0900

    Now jsonStorage.dev checks arguments types

--- jsonStorage/index.dev.js
+++ jsonStorage/index.dev.js
@@ -7,8 +7,35 @@ function jsonStorage(key) {
 
   const $this = new Map();
   const $key = key;
+  
+  // .dev-only
+  const dev_validate = (key, value) => {
+    if (typeof key !== 'string') {
+      throw new TypeError(`key has type ${typeof key} but expected string`);
+    }
+
+    if (
+      value === null
+      || ['boolean',
+          'number',
+          'string',
+          'object',
+        ].includes(typeof value)
+      || Array.isArray(value)
+    ) {
+      throw new TypeError(`value has type ${typeof value} but expected null, boolean, number, string, array or object`);
+    }
+  }
+
+  // .dev-only
+  $this.delete = key => (dev_validate(key, null), Map.prototype.delete.call($this, key));
+  $this.get = key => (dev_validate(key, null), Map.prototype.get.call($this, key));
+  $this.has = key => (dev_validate(key, null), Map.prototype.has.call($this, key));
+  $this.set = key => (dev_validate(key, value), Map.prototype.set.call($this, key, value));
+
 
   $this.save = key => (
+    dev_validate(key ?? $key, null), // .dev-only
     localStorage.setItem($key ?? key,
       JSON.stringify(
         Object.fromEntries($this) ?? {}
@@ -18,6 +45,7 @@ function jsonStorage(key) {
 
   $this.load = key => (
     (
+      dev_validate(key ?? $key, null), // .dev-only
       Object.entries(
         JSON.parse(
           localStorage.getItem($key ?? key)
